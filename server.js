2a5728dafa99b42226bbdf357f6980dbdbce7725
heyakhil <luvprogramming@gmail.com>
2025-02-07 14:22:49 +0530

    first commit v6

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-
+const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
